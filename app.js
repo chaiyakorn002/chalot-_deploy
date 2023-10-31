7d05a68e053a32a8e950b3eec9b65636c0f5edff
Chayakorn <chaiyakornton@gmail.com>
2023-11-01 06:00:54 +0700

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const multer = require('multer');
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
