cb21ee7ca130188806956b8ec3bfbbffe542ec0d
Chayakorn <chaiyakornton@gmail.com>
2023-10-31 23:31:44 +0700

    update

--- routes/uploadingimages.js
+++ routes/uploadingimages.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const multer = require('multer');
-const app = express()
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
 
 const storage = multer.diskStorage({
-destination: function (req, file, cb) {
-cb(null, 'uploads/')
-},
-filename: function (req, file, cb) {
-cb(null, Date.now() + ".png")
-}
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + ".png");
+  },
 })
-const upload = multer({ storage: storage })
-app.get('/up', (req, res) => {
-res.send('Hello Upload')
-})
-app.post('/upload', upload.single('file'),  (req, res) => { 
-    if (req.file) {
-        const imageUrl = req.file.filename;
-        res.status(200).json({ status: 'success', message: 'Image uploaded', imageUrl });
-      } else {
-    
-        res.status(400).json({ status: 'error', message: 'No file uploaded' });
-      }
-})
+
+const upload = multer({ storage: storage });
+
+router.post("/upload", upload.single("file"), (req, res) => {
+  if (req.file) {
+    const imageUrl = req.file.filename;
+    res
+      .status(200)
+      .json({ status: "success", message: "Image uploaded", imageUrl });
+  } else {
+    res.status(400).json({ status: "error", message: "No file uploaded" });
+  }
+});
+
+module.exports = router;
